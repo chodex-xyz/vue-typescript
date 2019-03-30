8482cb3e79946f4bfb0232919e752c3b04606da3
Konstantin Chernyshkov <xchode@gmail.com>
2019-03-30 23:04:04 +0500

    Adding an example of vuex-orm

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,20 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import VuexORM from "@vuex-orm/core";
+import Vuex from "vuex";
+
+import User from "./models/User";
+
+// Create a new database instance.
+const database = new VuexORM.Database();
+
+// Register Models to the database.
+database.register(User);
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  plugins: [VuexORM.install(database)]
 });
